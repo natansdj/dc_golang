7cf80e3b08f65258b14e312b88c6fcf4f970c014
Natan <natansdj@users.noreply.github.com>
2026-05-14 12:14:06 +0700

    update dockerfile

--- docker/golang/run.sh
+++ docker/golang/run.sh
@@ -23,8 +23,26 @@ if [ -z "$ENV" ]; then
     ENV="dev"
 fi
 
+if [ -z "$TARGET_GOOS" ]; then
+  TARGET_GOOS="linux"
+fi
+
+if [ -z "$TARGET_GOARCH" ]; then
+  TARGET_GOARCH="$(go env GOARCH)"
+fi
+
+if [ -z "$GO_BUILD_PATH" ]; then
+  GO_BUILD_PATH="."
+fi
+
+if [ -z "$GO_BUILD_OUTPUT" ]; then
+  GO_BUILD_OUTPUT=".svc-bin"
+fi
+
 #
-cd ${WORKDIR}
+if ! cd "$WORKDIR"; then
+  exit 1
+fi
 echo "WORKDIR : ${WORKDIR}"
 echo "GO_SVC : ${GO_SVC}"
 echo "ENV : ${ENV}"
@@ -43,10 +61,10 @@ fi
 if [ "$ENV" == "local" ]; then
   echo "## build & watch"
   # $GOBIN/bee run
-  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build && ./${GO_SVC}
+  CGO_ENABLED=0 GOOS=${TARGET_GOOS} GOARCH=${TARGET_GOARCH} go build -o "${GO_BUILD_OUTPUT}" "${GO_BUILD_PATH}" && ./${GO_BUILD_OUTPUT}
 elif [ "$ENV" == "dev" ] || [ "$ENV" == "staging" ]; then
   echo "## build & start go service"
-  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build && ./${GO_SVC}
+  CGO_ENABLED=0 GOOS=${TARGET_GOOS} GOARCH=${TARGET_GOARCH} go build -o "${GO_BUILD_OUTPUT}" "${GO_BUILD_PATH}" && ./${GO_BUILD_OUTPUT}
 else
   echo "## start go service"
   ./${GO_SVC}
